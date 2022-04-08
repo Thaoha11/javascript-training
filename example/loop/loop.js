bc07469d9c1aaef1f12f50edaddd8545049156eb
Thaoha11 <thaohaexo@gmail.com>
2022-04-09 00:48:41 +0700

    Add new loop folder

---
+++ example/loop/loop.js
@@ -0,0 +1,44 @@
+var myArray = [
+    'Thor',
+    'Tony',
+    'Natasha'
+]
+// while
+// var i = 0;
+
+// while (i < myArray.length) {
+//     console.log(myArray[i]);
+//     i++;
+// }
+// do..while
+// const list = ['a', 'b', 'c']
+// let j = 0
+
+// do {
+//     console.log(list[j]);
+//     j++;
+// } while (i < list.length)
+
+// for
+// const list = ['a', 'b', 'c']
+// for (let i = 0; i < list.length; i++) {
+//     console.log(list[i]);
+//     i++;
+// }
+
+//for..of
+// const list = ['a', 'b', 'c']
+// for (var value of Object.keys(list)) {
+//     console.log(value)
+// }
+// for (var value of Object.values(list)) {
+//     console.log(value)
+// }
+
+// for..in
+const list = ['a', 'b', 'c']
+for (var key in list) {
+    console.log(list[key])
+    console.log(key)
+
+}
