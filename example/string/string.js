82b2c937757606214f2b1157f98cc16897a0d498
Thaoha11 <thaohaexo@gmail.com>
2022-04-08 19:06:27 +0700

    Add all file

---
+++ example/string/string.js
@@ -0,0 +1,14 @@
+var myName = 'ha y thao'
+//length
+console.log(myName.length)
+
+//convert to lower case & upper case
+console.log(myName.toLocaleLowerCase())
+console.log(myName.toLocaleUpperCase())
+
+//slice
+console.log(myName.slice(2, 4))
+
+// indexOf
+const myString = 'javascript training '
+console.log(myString.indexOf('ing'))
