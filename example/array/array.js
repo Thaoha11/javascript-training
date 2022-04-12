1eb210a6be7dc31d84e83da21f62e241d4122bb0
Thaoha11 <thaohaexo@gmail.com>
2022-04-12 17:47:09 +0700

    Add method for array

--- example/array/array.js
+++ example/array/array.js
@@ -50,3 +50,54 @@ console.log(found)
 c.splice(1, 1, 'baby blue')
 console.log(c)
 
+// fill
+console.log(array1.fill(0, 2, 4))
+console.log(array1.fill(9, 1))
+console.log(array1.fill(4))
+
+//findIndex
+const array2 = [5, 12, 8, 130, 44];
+const isLargeNumber = (element) => element > 9;
+console.log(array2.findIndex(isLargeNumber));
+
+//entry
+const array3 = ['a', 'b', 'c']
+const iterator = array3.entries();
+console.log(iterator.next().value)
+console.log(iterator.next().value)
+
+//every
+const isBelowThreshold = (currentValue) => currentValue < 40;
+const array4 = [1, 30, 39, 29, 10, 13];
+console.log(array4.every(isBelowThreshold));
+
+//toString
+console.log(array3.toString())
+
+//sort
+console.log(array4.sort())
+
+//key
+const iterator1 = array3.keys();
+for (const key of iterator1) {
+    console.log(key);
+}
+
+// reduce
+const array5 = [1, 2, 3, 4];
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array5.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+);
+console.log(sumWithInitial);
+
+//reduceRight
+const array6 = [[0, 1], [2, 3], [4, 5]].reduceRight(
+    (accumulator, currentValue) => accumulator.concat(currentValue)
+);
+
+console.log(array6);
+
+
