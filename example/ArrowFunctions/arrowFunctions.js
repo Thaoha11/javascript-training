60cc6a782600337a1c21af99e7b08fd6edf7c2d7
Thaoha11 <thaohaexo@gmail.com>
2022-04-12 19:17:31 +0700

    Add example for Arrow function

---
+++ example/ArrowFunctions/arrowFunctions.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => a + b;
+console.log(sum(2, 3))
+
+const myString = (name, age) => ({ name: name, age: age });
+console.log(myString('Thao', '22'))
+
+const logger = log => console.log(log);
+logger('Message')
+
