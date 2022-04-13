546c653ddda6d6c49548015c6fbdaa0a80386ea8
Thaoha11 <thaohaexo@gmail.com>
2022-04-14 00:41:50 +0700

    Add example for date

---
+++ example/object/date.js
@@ -0,0 +1,11 @@
+//string 
+// var date = Date();
+
+//object
+var date = new Date();
+
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+
+console.log(`${day}/${month}/${year}`)
