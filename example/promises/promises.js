c8c9b88a77274d6a8ef68ee3337bb5ae7beb071c
Thaoha11 <thaohaexo@gmail.com>
2022-04-19 19:07:40 +0700

    Update example

--- example/promises/promises.js
+++ example/promises/promises.js
@@ -24,26 +24,49 @@
 //     })
 
 // Promise chain
-function sleep(ms) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, ms);
-    });
-}
+// function sleep(ms) {
+//     return new Promise(function (resolve) {
+//         setTimeout(resolve, ms);
+//     });
+// }
 
-sleep(1000)
-    .then(function () {
-        console.log(1);
-        return sleep(1000);
+// sleep(1000)
+//     .then(function () {
+//         console.log(1);
+//         return sleep(1000);
+//     })
+//     .then(function () {
+//         console.log(2);
+//         return sleep(1000);
+//     })
+//     .then(function () {
+//         console.log(3);
+//         return sleep(1000);
+//     })
+//     .then(function () {
+//         console.log(4);
+//         return sleep(1000);
+//     })
+
+let promise = new Promise(function (resolve) {
+    setTimeout(() => resolve(1), 1000)
+})
+    .then(function (result) {
+        alert(result); //1
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(result * 2), 1000)
+        })
+
     })
-    .then(function () {
-        console.log(2);
-        return sleep(1000);
+    .then(function (result) {
+        alert(result); //2
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(result * 2), 1000)
+        })
     })
-    .then(function () {
-        console.log(3);
-        return sleep(1000);
-    })
-    .then(function () {
-        console.log(4);
-        return sleep(1000);
+    .then(function (result) {
+        alert(result); //4
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(result * 2), 1000)
+        })
     })
