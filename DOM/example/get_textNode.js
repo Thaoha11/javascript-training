d1d423890d8e5ef3d980732eb614827929b0baa5
Thaoha11 <thaohaexo@gmail.com>
2022-04-21 18:17:02 +0700

    Create folder DOM

---
+++ DOM/example/get_textNode.js
@@ -0,0 +1,19 @@
+// InnerText is what we see on the screen
+// textContent is what is original in textNode
+
+var headingElement = document.querySelector('.heading')
+
+console.log(headingElement.innerText)
+
+console.log(headingElement.textContent)
+
+// headingElement.innerText = `
+
+//     New Heading 
+
+// `
+headingElement.textContent = `
+
+    New Heading 
+
+`
