e7ce8ca3b13236d0521cc15722780ba450dd468c
Thaoha11 <thaohaexo@gmail.com>
2022-05-18 17:15:05 +0700

    feat:Update addTodo

--- practice-1/src/models/todos.js
+++ practice-1/src/models/todos.js
@@ -10,28 +10,11 @@ export default class Model {
         this.onTodoListChanged = callback
     }
 
-    _commit(todos) {
-        this.onTodoListChanged(todos)
-        localStorage.setItem('todos', JSON.stringify(todos))
-
-    }
     /**
-     * 
-     * @param {string} id
+     * Use API url from fetch import and param todoText, id from controller in add todo
      * @param {string} todoText 
+     * @param {string} id 
      */
-    // addTodo(todoText) {
-    //     const todo = {
-    //         id: new Date().getTime().toString(),
-    //         text: todoText,
-    //         complete: false,
-    //     }
-
-    //     
-
-    //     this._commit(this.todos)
-
-    // }
 
     async addTodo(todoText) {
         await fetch.create(`/${path.PATH_TODO}`, {
@@ -39,44 +22,21 @@ export default class Model {
             text: todoText,
             complete: false,
         })
-        // this.todos.push(todo)
+
 
     }
 
+
+
+    /**
+   * Use API url from fetch import in read data
+   * @returns {array} todos.
+   */
     async getTodo() {
         const todo = await fetch.get(`/${path.PATH_TODO}`)
-        console.log('todo', todo)
+        // console.log('todo', todo)
         return todo
     }
-    /**
-     * 
-     * @param {string} id 
-     */
-    deleteTodo(id) {
-        this.todos = this.todos.filter(todo => todo.id !== id)
-
-        this._commit(this.todos)
-    }
-
-    // Edit todo
-    editTodo(id, updatedText) {
-        this.todos = this.todos.map(todo =>
-            todo.id === id ? { id: todo.id, text: updatedText, complete: todo.complete } : todo
-        )
-
-        this._commit(this.todos)
-    }
-
-    //Select
-
-    toggleTodo(id) {
-        this.todos.map(todo => {
-            todo.id === id ? { id: todo.id, text: todo.text, complete: !todo.complete } : todo
-
-        })
-        this._commit(this.todos)
-    }
-
 }
 
 
